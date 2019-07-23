4134affd535d5dd6e24833350d92dedea5df928a
Christopher Pugh <chris@virtuosoengine.com>
2019-07-22 22:22:04 -0400

    link color to fit the scheme.

--- source/Menu.js
+++ source/Menu.js
@@ -83,7 +83,7 @@ class Menu
         {
             if (this.options[x].enabled)
             {
-                let sb = "<pre><a href=\"javascript:game.gameInput('" + this.options[x].option + "');\">";
+                let sb = "<pre><a href=\"javascript:game.gameInput('" + this.options[x].option + "');\"  style=\"color: rgb(0,255,0)\">";
                 let sa = "</a></pre>"
                 rstring += sb + this.options[x] + sa;
                 console.log(sb + this.options[x] + sa);
